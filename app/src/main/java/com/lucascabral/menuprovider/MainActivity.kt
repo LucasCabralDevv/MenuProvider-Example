8617c6f36716d5fff7f11ac1b1a7f82b51b290f0
lucas <lucascabraldevv@gmail.com>
2022-01-05 19:56:07 -0300

    MenuProvider added.

--- app/src/main/java/com/lucascabral/menuprovider/MainActivity.kt
+++ app/src/main/java/com/lucascabral/menuprovider/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.lucascabral.menuprovider
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
 
-class MainActivity : ComponentActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem) = true
+        })
     }
 }
