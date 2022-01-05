6b200e9683285e41a5e628397428129058817821
lucas <lucascabraldevv@gmail.com>
2022-01-05 18:35:35 -0300

    init commit

---
+++ app/src/main/java/com/lucascabral/menuprovider/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lucascabral.menuprovider
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
